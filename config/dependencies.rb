0d012e01b37c87054bc1fee0c00a5ebbd2f4a01e
Matt McMahand <mmcmahand@gmail.com>
2008-12-29 00:03:11 -0500

    updated to merb 1.0.7. One of these days I'll get around to actually coding ;-)

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.6.1"
+merb_gems_version = "1.0.7"
 dm_gems_version   = "0.9.8"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
@@ -21,3 +21,5 @@ dependency "dm-migrations", dm_gems_version
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
+
+dependency "ruby-aaws", "0.4.4", :require_as => 'amazon/aws'
