9e5b0f534e7ea0e4db2c9141cb20e5995aa451b6
Matt McMahand <mmcmahand@gmail.com>
2009-06-24 16:26:11 -0400

    hpricot

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem 'hpricot'
   config.gem 'json'
   config.gem "newrelic_rpm"
 
