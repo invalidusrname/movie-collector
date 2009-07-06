a8ec50f6ca18d9ab5794093cbbc998d85b7c6f4c
Matt McMahand <mmcmahand@gmail.com>
2009-07-05 22:36:24 -0400

    added note about facebook canvas and fbconnect requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   include FacebookerAuthentication::Controller
 
-  before_filter :facebook_login_required, :if => :request_comes_from_facebook?
+  # FIXME: figure out how to handle facebook canvas and facebook connect using this filter
+  # before_filter :facebook_login_required, :if => :request_comes_from_facebook?
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
