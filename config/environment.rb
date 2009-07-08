c0075664a1f57b4c6bf2e36e05f3f8dd5d02c182
Matt McMahand <mmcmahand@gmail.com>
2009-07-08 01:43:52 -0400

    no reply address

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # $DEBUG = true
 
-DO_NOT_REPLY = "donotreply@example.com"
+DO_NOT_REPLY = APP_CONFIG['mail']['no_reply']
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
