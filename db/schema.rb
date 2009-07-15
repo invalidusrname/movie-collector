4f1a8843b1574cbcd5d116889c5eefeeb20e6a83
Matt McMahand <mmcmahand@gmail.com>
2009-07-15 14:24:19 -0400

    store sessions in the db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090623175149) do
+ActiveRecord::Schema.define(:version => 20090715162532) do
 
   create_table "box_office_films", :force => true do |t|
     t.string   "title"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(:version => 20090623175149) do
     t.string   "genre"
     t.string   "upc"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email"
