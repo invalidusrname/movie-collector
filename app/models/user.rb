dedb746fab017ba6e34f75c6bdf0a17655e5eb90
Matt McMahand <mmcmahand@gmail.com>
2009-09-02 15:06:43 -0400

    remove twitter functionality for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
-class User < TwitterAuth::GenericUser
+class User < ActiveRecord::Base
   include Clearance::User
   include FacebookerAuthentication::Model
+
   has_many :movies, :through => :users_movies
   has_many :users_movies
-  
+
   # todo: update validations from twitterauth when adding a twitter user
   def utilize_default_validations
     false
