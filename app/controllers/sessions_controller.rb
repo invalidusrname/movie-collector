47e90f7032f3071e9ae1606301b95561e31dc12e
Matt McMahand <mmcmahand@gmail.com>
2009-09-04 11:50:01 -0400

    incomplete feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,53 +10,54 @@ class SessionsController < Clearance::SessionsController
     render :template => '/sessions/new'
   end
 
-  def twitter_new
-    if ::TwitterAuth.oauth?
-      oauth_callback = request.protocol + request.host_with_port + '/oauth_callback'
-      @request_token = TwitterAuth.consumer.get_request_token({:oauth_callback=>oauth_callback})
-      session[:request_token] = @request_token.token
-      session[:request_token_secret] = @request_token.secret
-
-      url = @request_token.authorize_url
-      url << "&oauth_callback=#{CGI.escape(TwitterAuth.oauth_callback)}" if TwitterAuth.oauth_callback?
-      redirect_to url
-    end
-  end
-
-  def oauth_callback
-    unless session[:request_token] && session[:request_token_secret] 
-      authentication_failed('No authentication information was found in the session. Please try again.') and return
-    end
-  
-   unless params[:oauth_token].blank? || session[:request_token] ==  params[:oauth_token]
-     authentication_failed('Authentication information does not match session information. Please try again.') and return
-   end
-  
-    @request_token = OAuth::RequestToken.new(TwitterAuth.consumer, session[:request_token], session[:request_token_secret])
-  
-    oauth_verifier = params["oauth_verifier"]
-    @access_token = @request_token.get_access_token(:oauth_verifier => oauth_verifier)
-    
-    # The request token has been invalidated
-    # so we nullify it in the session.
-    session[:request_token] = nil
-    session[:request_token_secret] = nil
-
-    user = User.identify_or_create_from_access_token(@access_token)
-    user.update_attribute(:email_confirmed, true)
-
-    cookies[:remember_token] = user.remember_me
-    sign_in(user)
-    set_current_user
-    authentication_succeeded 
-  rescue Net::HTTPServerException => e
-    case e.message
-      when '401 "Unauthorized"'
-        authentication_failed('This authentication request is no longer valid. Please try again.') and return
-      else
-        authentication_failed('There was a problem trying to authenticate you. Please try again.') and return
-    end 
-  end
+# TODO: commenting out until feature is complete
+#  def twitter_new
+#   if ::TwitterAuth.oauth?
+#     oauth_callback = request.protocol + request.host_with_port + '/oauth_callback'
+#     @request_token = TwitterAuth.consumer.get_request_token({:oauth_callback=>oauth_callback})
+#     session[:request_token] = @request_token.token
+#     session[:request_token_secret] = @request_token.secret
+#
+#     url = @request_token.authorize_url
+#     url << "&oauth_callback=#{CGI.escape(TwitterAuth.oauth_callback)}" if TwitterAuth.oauth_callback?
+#     redirect_to url
+#   end
+# end
+# 
+ # def oauth_callback
+ #   unless session[:request_token] && session[:request_token_secret] 
+ #     authentication_failed('No authentication information was found in the session. Please try again.') and return
+ #   end
+ # 
+ #   unless params[:oauth_token].blank? || session[:request_token] ==  params[:oauth_token]
+ #     authentication_failed('Authentication information does not match session information. Please try again.') and return
+ #   end
+ #  
+ #    @request_token = OAuth::RequestToken.new(TwitterAuth.consumer, session[:request_token], session[:request_token_secret])
+ #  
+ #    oauth_verifier = params["oauth_verifier"]
+ #    @access_token = @request_token.get_access_token(:oauth_verifier => oauth_verifier)
+ #    
+ #    # The request token has been invalidated
+ #    # so we nullify it in the session.
+ #    session[:request_token] = nil
+ #    session[:request_token_secret] = nil
+ # 
+ #    user = User.identify_or_create_from_access_token(@access_token)
+ #    user.update_attribute(:email_confirmed, true)
+ # 
+ #    cookies[:remember_token] = user.remember_me
+ #    sign_in(user)
+ #    set_current_user
+ #    authentication_succeeded 
+ #  rescue Net::HTTPServerException => e
+ #    case e.message
+ #      when '401 "Unauthorized"'
+ #        authentication_failed('This authentication request is no longer valid. Please try again.') and return
+ #      else
+ #        authentication_failed('There was a problem trying to authenticate you. Please try again.') and return
+ #    end 
+ #  end
 
   def facebook_new
     # handle the case where ensure_authenticated_to_facebook filter passes through,
