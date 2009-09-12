6b7032dbef5afb14d0cb1939a25f056113eb00dc
Matt McMahand <mmcmahand@gmail.com>
2009-09-12 12:58:37 -0400

    route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.root :controller => 'home'
+
   map.resources :movies
   map.resources :users_movies, :collection => {:recently_added => :get}, :member => {:add_rating => :post}
-
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'home'
 
   map.login  '/login',  :controller => 'Sessions', :action => 'new'
   map.logout '/logout', :controller => 'Sessions', :action => 'destroy'
@@ -48,15 +16,10 @@ ActionController::Routing::Routes.draw do |map|
   map.my_movies '/my_movies', :controller => 'UsersMovies'
   map.friends_movies '/friends_movies', :controller => 'UsersMovies', :action => 'friends'
 
-  # See how all your routes lay out with "rake routes"
-
   map.facebook_post_install '/facebook_welcome', :controller => 'Home'
   map.oauth_callback '/oauth_callback', :controller => 'sessions', :action => 'oauth_callback'
   map.twitter_oauth '/oauth/twitter/sign_in', :controller => 'sessions', :action => 'twitter_new'
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
