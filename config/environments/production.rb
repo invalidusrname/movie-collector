276d675ad9711c4e1e19fbf919e82768f7eca5b7
Matt McMahand <mmcmahand@gmail.com>
2010-08-29 22:38:29 -0400

    add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ MovieCollector::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -46,4 +45,7 @@ MovieCollector::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  HOST = 'moviecollector.org'
+  
 end
