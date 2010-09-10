e5ed215cf5d68577651752af2e8b4f8156f11fb1
Matt McMahand <mmcmahand@gmail.com>
2010-09-09 23:30:43 -0400

    .

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -4,12 +4,14 @@
 
 # Create the dir
 require 'fileutils'
-FileUtils.mkdir_p('/tmp/stylesheets')
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
 
 Sass::Plugin.on_updating_stylesheet do |template, css|
   puts "Compiling #{template} to #{css}"
 end
 
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-                                             :urls => ['/stylesheets'],
-                                             :root => "/tmp")
+ActionController::Dispatcher.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets/compiled"])
+
+# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+                                             # :urls => ['/stylesheets'],
+                                             # :root => "/tmp")
