d83d58e0e91ec4c63e9d1d796bc5688878799cd3
Matt McMahand <mmcmahand@gmail.com>
2010-09-09 23:31:54 -0400

    ..

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -10,8 +10,6 @@ Sass::Plugin.on_updating_stylesheet do |template, css|
   puts "Compiling #{template} to #{css}"
 end
 
-ActionController::Dispatcher.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets/compiled"])
-
-# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-                                             # :urls => ['/stylesheets'],
-                                             # :root => "/tmp")
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+                                             :urls => ['/stylesheets/compiled'],
+                                             :root => "/tmp")
