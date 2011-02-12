4080fce450d9e8a004beea7c76a71b826ed78813
Matt McMahand <mmcmahand@gmail.com>
2011-02-12 02:11:48 -0500

    ruby 1.9.2 updates

--- app/models/box_office_film.rb
+++ app/models/box_office_film.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class BoxOfficeFilm < ActiveRecord::Base
   scope :top_films, :conditions => ['position is not null'], :order => :position
   scope :this_week, lambda { {:conditions => ['release_date BETWEEN ? AND ?', Date.today.beginning_of_week, Date.today.end_of_week], :order => 'release_date DESC' } }
