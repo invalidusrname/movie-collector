946526f3888db6dad0f6b4410827f894e873d327
Matt McMahand <mmcmahand@MBPro-2.local>
2011-03-03 19:55:52 -0500

    fixing compass errors based on heroku's recommendations (http://devcenter.heroku.com/articles/using-compass)

--- config.ru
+++ config.ru
@@ -1,9 +1,16 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
+
+
+if Rails.env == "production"
+  use Rack::Static, :urls => ["/stylesheets/compiled"], :root => "tmp" #FOR COMPASS
+end
+
 run MovieCollector::Application
 
 # ENV['RAILS_ENV'] = ENV['RACK_ENV'] || 'development'
+
 
 # use Rack::Tidy
 # use Rails::Rack::LogTailer
--- config/compass.rb
+++ config/compass.rb
@@ -2,10 +2,13 @@
 # Require any additional compass plugins here.
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
+environment  = Compass::AppIntegration::Rails.env
+
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "tmp/stylesheets/compiled"
-sass_dir = "app/stylesheets"
-environment = Compass::AppIntegration::Rails.env
+
+css_dir = "tmp/stylesheets"
+sass_dir = "app/views/stylesheets"
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
