71f75caf4cdbb5954e352932bebfd55c099c29fb
Matt McMahand <mmcmahand@MBPro-2.local>
2011-03-05 23:19:20 -0500

    remove duplicate compass initializer actually initialize compass (via Compass::AppIntegration::Rails.initialize!)

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,8 @@
 require 'fileutils'
+
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Compass::AppIntegration::Rails.initialize!
 
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
     :urls => ['/stylesheets'],
