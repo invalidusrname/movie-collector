e82f3e56d245af94e2c02cde1c2a368c90983258
Matt McMahand <matt@invalid8.com>
2022-12-21 13:02:34 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
-# TODO: fix clearance and facebook routes
+# frozen_string_literal: true
+
 MovieCollector::Application.routes.draw do
-  resource :session do
-    # get :facebook_new
-    # get :facebook_create
-  end
+  resource :session
 
   resources :movies do
     collection do
@@ -15,24 +13,23 @@ MovieCollector::Application.routes.draw do
     collection do
       get :recently_added
     end
+
     member do
       post :add_rating
     end
   end
 
-  get '/box_office', to:'box_office_films#index', :as, 'box_office'
-  get '/my_movies', to:'users_movies#index', :as, 'my_movies'
-  get '/friends_movies', to:'users_movies#friends', :as, 'friends_movies'
+  get "/box_office", to: "box_office_films#index", as: "box_office"
+  get "/my_movies", to: "users_movies#index", as: "my_movies"
+  get "/friends_movies", to: "users_movies#friends", as: "friends_movies"
 
   # authentication
-  get '/login' , to:'sessions#new',     :as, 'login'
-  get '/login' , to:'sessions#new',     :as, 'sign_in'
-  get '/logout', to:'sessions#destroy', :as, 'logout'
-  get '/signup', to:'Clearance::Users#new', :as, 'signup'
-  get '/create_user', to:'Clearance::Users#create', :as, 'create_user'
-  get '/forgot_password', to:'Clearance::Passwords#new', :as, 'forgot_password'
+  get "/login", to: "sessions#new",     as: "login"
+  get "/login", to: "sessions#new",     as: "sign_in"
+  get "/logout", to: "sessions#destroy", as: "logout"
+  get "/signup", to: "Clearance::Users#new", as: "signup"
+  get "/create_user", to: "Clearance::Users#create", as: "create_user"
+  get "/forgot_password", to: "Clearance::Passwords#new", as: "forgot_password"
 
   root "home#index"
-
-  get ':controller(/:action(/:id(.:format)))'
 end
