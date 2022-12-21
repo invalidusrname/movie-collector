3a32dfac6a98f7e5b08466d4274d40009830a0c6
Matt McMahand <matt@invalid8.com>
2022-12-21 13:02:12 -0500

    remove hoptoad

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -2,6 +2,3 @@
 
 # APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
 # DO_NOT_REPLY = APP_CONFIG['mail']['no_reply']
-
-ENV["AMAZONRCDIR"] = "#{Rails.root}/config"
-ENV["AMAZONRCFILE"] = "amazonrc.txt"
