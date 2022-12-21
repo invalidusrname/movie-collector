c5ae12dc88cf78f5b2cf62ad35f90b24d58b2906
Matt McMahand <matt@invalid8.com>
2022-12-21 13:44:24 -0500

    use rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ Bundler.require(*Rails.groups)
 
 module MovieCollector
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -19,6 +18,5 @@ module MovieCollector
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
