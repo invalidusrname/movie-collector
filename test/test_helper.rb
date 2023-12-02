eb8db90084ad4b928d5106c9188df7476675fabb
Matt McMahand <matt@invalid8.com>
2023-12-02 15:55:01 -0500

    debugging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ENV["RAILS_ENV"] = "test"
-require File.expand_path("../config/environment", __dir__)
+require_relative "../config/environment"
 require "rails/test_help"
 
 require "clearance/test_unit"
