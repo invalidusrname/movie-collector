db1989cfae0194ba1782b8c4fe079f6ade4451d4
Matt <invalidusrname@users.noreply.github.com>
2023-12-02 16:00:41 -0500

    Update test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ENV["RAILS_ENV"] = "test"
-require_relative "../config/environment"
+require File.expand_path("../config/environment", __dir__)
 require "rails/test_help"
 
 require "clearance/test_unit"
